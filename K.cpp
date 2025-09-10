acbdfac21ef52109e9ecf9af7eb2c9afb06fd852
Abdulrahim Khoja <r7moni.rhmoni@gmail.com>
2025-09-11 00:20:10 +0200

    Create K.cpp

---
+++ K.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#define IOS ios_base::sync_with_stdio(false);cin.tie(nullptr);cout.tie(nullptr);
+#define input(arr) for(auto& index : arr){cin>>index;}
+#define output(arr) for(auto& index : arr){cout<<index<<' ';}
+#define show(x) cout<<#x<<" = "<<x<<endl;
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define pb push_back
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vector<int>>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using pi = pair<int, int>;
+template <typename T>
+using pqpair = priority_queue<T, vector<T>, greater<T>>;
+
+void solve() {
+    int n;
+    cin>>n;
+    vi a(n);
+    input(a);
+    int res=0;
+    for (int i=0; i<n; i++) {
+        int cnt=-1, frq[1005]{};
+        for (int j=i; j<n; j++) {
+            cnt++;
+            if (frq[a[j]]) {
+                cnt--;
+            }
+            else {
+                if (frq[a[j]+1]) {
+                    cnt--;
+                }
+                if (frq[a[j]-1]) {
+                    cnt--;
+                }
+            }
+            frq[a[j]]++;
+            res+=cnt;
+        }
+    }
+    cout<<res;
+}
+
+
+int main() {
+    IOS
+
+    int t=1;
+    cin>>t;
+    while (t--) {
+        solve();
+        cout<<(t?"\n":"");
+    }
+}
